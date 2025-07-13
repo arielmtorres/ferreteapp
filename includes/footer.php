291c75f41032a733e9f2f28eeb3698d526dc6fb7
Pintos Machado Lara Aylen <larapintos.61@gmail.com>
2025-07-13 20:15:37 -0300

    actualice el nav
    ahora es resposive, le cambie el icono, y le puse un efecto a los botones

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,11 @@
   </div> <!-- /.container -->
   <!-- Scripts bootstrap -->
   <script  src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  <script>
+    document.querySelector('.menu-toggle').addEventListener('click', () => {
+      document.getElementById('userMenu').classList.toggle('open');
+    });
+  </script>
 
   
 <!-- Chart.js PRIMERO -->
--- includes/header.php
+++ includes/header.php
@@ -17,18 +17,18 @@ if (!defined('FERRETEAPP')) exit;
   <link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
- 
   <header>
     <div class="header-left">
-      <img src="img/header.png" alt="Logo" class="logo" />
-     
+      <img src="img/logoF.png" alt="Logo" class="logo" />
     </div>
 
-    <div class="user-menu">
-      <!-- avatar, nombre, dropdown -->
+    <button class="menu-toggle" aria-label="Abrir menú">
+      ☰
+    </button>
+
+    <div class="user-menu" id="userMenu">
       <?php include __DIR__.'/nav.php'; ?>
     </div>
-
-    
   </header>
+
   <div class="container">
