be3448e0511a96f574ff540c742beace7224ec3c
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-11-02 20:18:51 -0300

    Add Reportes

--- includes/nav.php
+++ includes/nav.php
@@ -6,6 +6,5 @@
   <a href="#/stock"      class="btn btn-link">Stock</a>
   <a href="/ferreteApp/components/egresosEingresos/egresosEingresos.php"  class="btn btn-link">Egresos/Ingresos</a>
   <a href="#/ventas"     class="btn btn-link">Ventas</a>
-  <a href="#/reportes"   class="btn btn-link">Reportes</a>
   <a href="/ferreteApp/components/usuarios/usuarios.php"   class="btn btn-link">Usuarios</a>
 </nav>
