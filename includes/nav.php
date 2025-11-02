8c58b374f22c73b4d5a429a9c24b5fa2e7a4a05e
Pintos Machado Lara Aylen <larapintos.61@gmail.com>
2025-11-02 20:28:36 -0300

    Revert "Merge branch 'main' of https://github.com/arielmtorres/ferreteapp"
    This reverts commit 646a138da3b147c8235f5742648bc037508d4126, reversing
    changes made to 01aabef02212ad5a78eb03f8bacd3b6258e974ff.

--- includes/nav.php
+++ includes/nav.php
@@ -6,6 +6,6 @@
   <a href="#/stock"      class="btn btn-link">Stock</a>
   <a href="/ferreteApp/components/egresosEingresos/egresosEingresos.php"  class="btn btn-link">Egresos/Ingresos</a>
   <a href="#/ventas"     class="btn btn-link">Ventas</a>
-  <a href="https://lookerstudio.google.com/reporting/b9a5d4d2-abce-4aa5-8009-0a25c5108998"   class="btn btn-link">Reportes</a>
+  <a href="#/reportes"   class="btn btn-link">Reportes</a>
   <a href="/ferreteApp/components/usuarios/usuarios.php"   class="btn btn-link">Usuarios</a>
 </nav>
