c2e1aab47954c32276c3c12848b215024f1b999c
arielmtorres <ariel.mauricio.torres83@gmail.com>
2025-11-03 19:36:38 -0300

    act

--- includes/nav.php
+++ includes/nav.php
@@ -6,6 +6,6 @@
   <a href="#/stock"      class="btn btn-link">Stock</a>
   <a href="/ferreteApp/components/egresosEingresos/egresosEingresos.php"  class="btn btn-link">Egresos/Ingresos</a>
   <a href="#/ventas"     class="btn btn-link">Ventas</a>
-  <a href="#/reportes"   class="btn btn-link">Reportes</a>
+  <a href="https://lookerstudio.google.com/s/tI6_6lrlRTM"   class="btn btn-link">Reportes</a>
   <a href="/ferreteApp/components/usuarios/usuarios.php"   class="btn btn-link">Usuarios</a>
 </nav>
